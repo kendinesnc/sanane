8c99c538704638da580e968994eeb3d9ba91ea4b
sunac01 <82957587+sunac01@users.noreply.github.com>
2021-05-31 21:13:14 +0300

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -8,17 +8,21 @@ from helpers.filters import other_filters2
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""Ben **{bn}**
-Kullanım : /e < Şarkı Adı >
+        f"""I **{bn}**
+🇬🇧 Usage : /e Music Name
+🇹🇷 Kullanım : /e Müzik Adı
+🇬🇧 Sorry I Only Work in Groups :(
+🇹🇷 Üzgünüm Sadece Gruplarda Çalışırım :(
+
         """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "Sahibi 💬", url="https://t.me/Azerbesk"
+                        "📣 Channel 📣", url="https://t.me/NetdBots"
                     ),
                     InlineKeyboardButton(
-                        "Kanal 📣", url="https://t.me/KaybedenlerOrkestrasi"
+                        "➕ Beni Bir Grupta Dene ➕", url="https://t.me/NetdMusicbot?startgroup=true"
                     )
                 ]
             ]
