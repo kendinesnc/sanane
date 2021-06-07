d9a6abb4646879f0e8c734d42f50f0c84555bbd6
sunac01 <82957587+sunac01@users.noreply.github.com>
2021-06-07 06:27:33 +0300

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -18,7 +18,7 @@ async def a(client, message: Message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = await message.reply(f"**{Bn} :-** 🔍 Aranıyor {query}")
+    m = await message.reply(f"**{Bn} :-** 🦸🏼‍♂️ Aranıyor {query}")
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = []
@@ -55,7 +55,7 @@ async def a(client, message: Message):
         )
         print(str(e))
         return
-    await m.edit(f"**{Bn} :-** 📥 Indiriyor...\n**Query :-** {query}")
+    await m.edit(f"**{Bn} :-** 🧞 İndiriyor...\n**Aranan :** {query}")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
